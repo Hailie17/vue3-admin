a45e502190c1d747ca9463429386ee912f2b093f
Hailie17 <192151452@qq.com>
2025-02-19 01:03:12 +0800

    导航栏搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <!-- 也一样 -->
+  <div class="app-wrapper">
+    <div class="sidebar-container">侧导航栏</div>
+    <div class="main-container">
+      <div class="header">导航条1</div>
+      <div class="tags-view">导航条2</div>
+    </div>
+    <div class="app-main">
+      <router-view></router-view>
+    </div>
+
+  </div>
 </template>
