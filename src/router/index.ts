835aa9c7ced10f59b0a14c5e4c6065d6aee26cd7
Hailie17 <192151452@qq.com>
2025-02-20 22:46:05 +0800

    unocss 导入与使用

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,14 +14,7 @@ const routes: RouteRecordRaw[] = [
       }
     ]
   },
-  {
-    path: '/home',
-    component: () => import('@/views/Home.vue')
-  },
-  {
-    path: '/about',
-    component: () => import('@/views/About.vue')
-  }
+  
 ]
 
 export default createRouter({
---
+++ uno.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'unocss'
+import  presetAttributify from '@unocss/preset-attributify'
+import presetUno from '@unocss/preset-uno'
+
+export default defineConfig({
+  presets: [presetAttributify(), presetUno()]
+})
