48b27b7ca25900ac6a8743f55c05e56367ae8b19
Hailie17 <192151452@qq.com>
2025-02-24 23:29:58 +0800

    04 elementplus 按需导入

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +14,11 @@ export default defineConfig({
       replacement: path.resolve(__dirname, 'src')
     }]
   },
-  plugins: [vue(), Unocss()],
+  plugins: [vue(), Unocss(), 
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),],
 })
