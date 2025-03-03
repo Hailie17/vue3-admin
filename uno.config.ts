312cd0b1370813c4f3db6f0430ffa3b2a6ee187c
Hailie17 <192151452@qq.com>
2025-03-04 00:08:18 +0800

    05 安装字体图标组件库

--- uno.config.ts
+++ uno.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'unocss'
 import  presetAttributify from '@unocss/preset-attributify'
 import presetUno from '@unocss/preset-uno'
 import transformerDirectives from '@unocss/transformer-directives'
-
+import presetIcons from '@unocss/preset-icons'
 
 export default defineConfig({
-  presets: [presetAttributify(), presetUno()],
+  presets: [presetAttributify(), presetUno(), presetIcons()],
   transformers: [transformerDirectives()] // apply类似写法
 })
