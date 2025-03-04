9d1fdb7ebe74b52ce341955313b3cc2c3c64e94f
Hailie17 <192151452@qq.com>
2025-03-04 22:12:49 +0800

    使用unocss图标库ant-design

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,16 +1,16 @@
 <template>
   <el-button>button</el-button>
-  <el-button @click="handle">open</el-button>
+  <div i-ant-design:apple-filled></div>
 </template>
 
 <script lang="ts" setup>
 // 服务型组件 放到 示例上调用
-const { proxy } = getCurrentInstance()!
-const handle = () => {
-  proxy?.$message('Hi My dear')
-}
+// const { proxy } = getCurrentInstance()!
+// const handle = () => {
+//   proxy?.$message('Hi My dear')
+// }
 
-const res = ref(0)
-console.log('res', res.value);
+// const res = ref(0)
+// console.log('res', res.value);
 
 </script>
--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,8 @@ import  presetAttributify from '@unocss/preset-attributify'
 import presetUno from '@unocss/preset-uno'
 import transformerDirectives from '@unocss/transformer-directives'
 import presetIcons from '@unocss/preset-icons'
-
+// unocss图标会预设查找依赖的图标库
+// ant-design
 export default defineConfig({
   presets: [presetAttributify(), presetUno(), presetIcons()],
   transformers: [transformerDirectives()] // apply类似写法
