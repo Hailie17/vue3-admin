0f85070552561eac2aefe27584b1aa691511ec5c
Hailie17 <192151452@qq.com>
2025-03-06 22:24:37 +0800

    设置默认属性值

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,10 +1,17 @@
+<template>
+  <IconifyIcon :class="svgClass" :icon="iconName" v-if="!isExt" v-bind="$attrs"></IconifyIcon>
+  <div v-else :style="styleExternalIcon" class="svgClass" bg-current v-bind="$attrs"></div>
+</template>
+
 <script setup lang="ts">
 import {isExternal} from '@/utils/validate'
 import {Icon as IconifyIcon} from '@iconify/vue'
-const { iconName, customClass} = defineProps<{
+const { iconName, customClass} = withDefaults(defineProps<{
   iconName: string,
   customClass: string
-}>()
+}>(),{
+  customClass: ''
+})
 // 组合自定义类名
 const svgClass = computed(() =>  {
   customClass ? `icon ${customClass}` : 'icon'
@@ -18,12 +25,9 @@ const styleExternalIcon = computed(() => ({
   "-webkit-mask": `url(${iconName}) norepeat 50% 50%`,
   "mask-size": "cover"
 }))
-</script>
 
-<template>
-  <IconifyIcon :class="svgClass" :icon="iconName" v-if="!isExt" v-bind="$attrs"></IconifyIcon>
-  <div v-else :style="styleExternalIcon" class="svgClass" bg-current v-bind="$attrs"></div>
-</template>
+// 在实现图标的时候，尽量使用svg，不要使用font字体--页面放大会出现锯齿，而且不圆润
+</script>
 
 <style scoped>
 .read-the-docs {
