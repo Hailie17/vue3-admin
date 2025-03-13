6a7a31fd8c5aed92e58f21cd74f521f91c1bad2f
Hailie17 <192151452@qq.com>
2025-03-13 23:54:26 +0800

    导航栏菜单

--- components.d.ts
+++ components.d.ts
@@ -9,7 +9,9 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-menu router default-active="'/dashboard'"></el-menu>
+  <el-menu router default-active="'/dashboard'" :background-color="varaibles.menuBg" :text-color="varaibles.menuText" :active-text-color="varaibles.menuActiveText">
+    <el-menu-item index="/dashboard">
+      <el-icon><setting /></el-icon>
+      <template #title>Navigator Four</template>
+    </el-menu-item>
+  </el-menu>
 </template>
 
 <script setup> 
