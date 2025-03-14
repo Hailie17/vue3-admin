b49294544600febe4269e3f2e0a9b296ef6ded3c
Hailie17 <192151452@qq.com>
2025-03-14 23:47:34 +0800

    面包屑点击事件，prettier文件更正

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="navbar">
-    <hamburger></hamburger>
+    <hamburger @click="handleCollapse" :collapse="collapse"></hamburger>
+    <div></div>
   </div>
 </template>
+
+<script setup>
+const collapse = ref(false)
+const handleCollapse = () => {
+  collapse.value = !collapse.value
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  @apply h-[var(--navbar-height)];
+}
+</style>
