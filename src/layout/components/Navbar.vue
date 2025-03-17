0fc5665f95bc57a88010b1ea143e63d2fe187620
Hailie17 <192151452@qq.com>
2025-03-17 22:27:19 +0800

    菜单状态存store。从store共享状态

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="navbar">
-    <hamburger @toggleCollapse="handleCollapse" :collapse="collapse"></hamburger>
+    <hamburger @toggleCollapse="toggleSidebar" :collapse="sidebar.opened"></hamburger>
     <div></div>
   </div>
 </template>
 
 <script setup>
-const collapse = ref(false)
-const handleCollapse = () => {
-  collapse.value = !collapse.value
-}
+import { useAppStore } from '@/stores/app'
+// 结构时要考虑值是不是对象，非对象结构会丧失响应式
+const { toggleSidebar, sidebar } = useAppStore()
+// const collapse = ref(false)
+// const handleCollapse = () => {
+//   collapse.value = !collapse.value
+// }
 </script>
 
 <style lang="scss" scoped>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <el-menu router class="sidebar-container-menu" :default-active="defaultActive" :background-color="variables.menuBg" :text-color="variables.menuText" :active-text-color="variables.menuActiveText" :collapse="collapse">
+  <el-menu
+    router
+    class="sidebar-container-menu"
+    :default-active="defaultActive"
+    :background-color="variables.menuBg"
+    :text-color="variables.menuText"
+    :active-text-color="variables.menuActiveText"
+    :collapse="sidebar.opened"
+  >
     <el-button @click="collapse = !collapse"></el-button>
     <el-menu-item index="/dashboard">
       <el-icon><setting /></el-icon>
@@ -8,13 +16,15 @@
   </el-menu>
 </template>
 
-<script setup> 
-  import variables from '@/style/variables.module.scss'
+<script setup>
+import { useAppStore } from '@/stores/app'
+import variables from '@/style/variables.module.scss'
 
-  const route = useRoute()
-  const defaultActive = computed(() => {
-    return route.path
-  })
+const route = useRoute()
+const defaultActive = computed(() => {
+  return route.path
+})
 
-  const collapse = ref(false)
+const { sidebar } = useAppStore()
+// const collapse = ref(false)
 </script>
