597adf6c83e604a416d35d6cd81d7721788f27fa
Hailie17 <192151452@qq.com>
2025-03-19 22:32:57 +0800

    实现点击菜单页面路由切换

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-menu-item v-if="filteredChildren.length <= 1">
+  <el-menu-item v-if="filteredChildren.length <= 1" :index="resolvePath(singleChildRoute.path)">
     <el-icon v-if="iconName">
       <svg-icon :icon-name="iconName" />
     </el-icon>
@@ -12,12 +12,18 @@
       </el-icon>
       <span>{{ singleChildRoute.meta?.title }}</span>
     </template>
-    <sidebar-item v-for="child of filteredChildren" :key="child.path" :item="child"></sidebar-item>
+    <sidebar-item
+      v-for="child of filteredChildren"
+      :base-path="basePath"
+      :key="child.path"
+      :item="child"
+    ></sidebar-item>
   </el-sub-menu>
 </template>
 <script lang="ts" setup>
 import type { RouteRecordRaw } from 'vue-router'
-const { item } = defineProps<{ item: RouteRecordRaw }>()
+import path from 'path-browserify'
+const { item, basePath } = defineProps<{ item: RouteRecordRaw; basePath: string }>()
 // 根据路由配置循环
 // 如果只有一个子菜单，直接渲染这个子菜单即可
 // 如果有多个子菜单，使用el-submenu去渲染
@@ -28,4 +34,7 @@ const singleChildRoute = computed(() => (filteredChildren.value.length === 1 ? f
 
 // 要渲染的图标
 const iconName = computed(() => singleChildRoute.value.meta?.icon)
+
+// 解析父路径 + 子路径，resolve可以解析绝对路径 父路径/system 子路径/system/menu
+const resolvePath = (childPath: string) => path.resolve(basePath, childPath)
 </script>
