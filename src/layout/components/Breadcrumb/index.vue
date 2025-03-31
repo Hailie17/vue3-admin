42db3ef963937422a3515f09525113ca72b71cb9
Hailie17 <192151452@qq.com>
2025-03-31 23:27:10 +0800

    面包屑渲染路由

--- src/layout/components/Breadcrumb/index.vue
+++ src/layout/components/Breadcrumb/index.vue
@@ -1,12 +1,32 @@
 <template>
   <el-breadcrumb separator="/" leading-50px text-lg ml-30px inline-block>
-    <el-breadcrumb-item v-for="(a, index) in arr">
-      <span v-if="index === arr.length - 1">{{ a }}</span>
-      <a v-else>{{ a }}</a>
+    <el-breadcrumb-item v-for="(a, index) in list" :to="a.path">
+      <span v-if="index === list.length - 1">{{ a.meta?.title }}</span>
+      <a v-else>{{ a.meta?.title }}</a>
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script lang="ts" setup>
-const arr = [1, 2, 3]
+import { type RouteLocationMatched } from 'vue-router'
+const route = useRoute()
+const list = ref<Partial<RouteLocationMatched>[]>([])
+
+const getBreadCrumb = () => {
+  let matched = route.matched.filter((metch) => metch.meta.title) as Partial<RouteLocationMatched>[]
+  if (matched[0].path !== '/dashboard') {
+    matched = [
+      {
+        path: '/dashboard',
+        meta: {
+          title: 'dashboard',
+        },
+      },
+      ...matched,
+    ]
+  }
+  list.value = matched
+}
+
+watch(() => route.path, getBreadCrumb, { immediate: true })
 </script>
