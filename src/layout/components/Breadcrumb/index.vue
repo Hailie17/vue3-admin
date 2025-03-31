a83764a24d26aad51f0810551082a038866566a3
Hailie17 <192151452@qq.com>
2025-03-31 22:41:21 +0800

    面包屑 最后一个无法点击跳转，前面的导航栏可以点击跳转

--- src/layout/components/Breadcrumb/index.vue
+++ src/layout/components/Breadcrumb/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <el-breadcrumb separator="/">
-    <el-breadcrumb-item v-for="a in 3">{{ a }}</el-breadcrumb-item>
+  <el-breadcrumb separator="/" leading-50px text-lg ml-30px inline-block>
+    <el-breadcrumb-item v-for="(a, index) in arr">
+      <span v-if="index === arr.length - 1">{{ a }}</span>
+      <a v-else>{{ a }}</a>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
+
+<script lang="ts" setup>
+const arr = [1, 2, 3]
+</script>
