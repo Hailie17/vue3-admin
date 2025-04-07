4ccf03a588823d059f67c3c182423ee89bb36189
Hailie17 <192151452@qq.com>
2025-04-07 23:14:58 +0800

    状态栏扩大缩小按钮图标控制

--- src/layout/components/Screenfull/index.vue
+++ src/layout/components/Screenfull/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <svg-icon icon-name="ant-design:fullscreen-exit-outlined"></svg-icon>
+  <svg-icon
+    custom-class="w-2em h-2em"
+    :icon-name="isFullScreen ? 'ant-design:fullscreen-exit-outlined' : 'ant-design:fullscreen-outlined'"
+  ></svg-icon>
 </template>
+<script lang="ts" setup>
+const isFullScreen = ref(false)
+</script>
