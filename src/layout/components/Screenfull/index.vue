200d2773e05b3e8ad5b15c81cdacbdb699a6020c
Hailie17 <192151452@qq.com>
2025-04-07 23:16:37 +0800

    点击切换图标

--- src/layout/components/Screenfull/index.vue
+++ src/layout/components/Screenfull/index.vue
@@ -1,9 +1,13 @@
 <template>
   <svg-icon
+    @click="handleClick"
     custom-class="w-2em h-2em"
     :icon-name="isFullScreen ? 'ant-design:fullscreen-exit-outlined' : 'ant-design:fullscreen-outlined'"
   ></svg-icon>
 </template>
 <script lang="ts" setup>
 const isFullScreen = ref(false)
+function handleClick() {
+  isFullScreen.value = !isFullScreen.value
+}
 </script>
