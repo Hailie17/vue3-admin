077a7781ccdc0e4b46d3d4de3aec4402f75a9ffb
Hailie17 <192151452@qq.com>
2025-04-14 01:10:31 +0800

    用户设置持久化保存

--- src/stores/setting.ts
+++ src/stores/setting.ts
@@ -1,16 +1,26 @@
 // 设置用户主题、颜色、配置
 import variables from '@/style/variables.module.scss'
-export const useSettingStore = defineStore('setting', () => {
-  // variables
-  // 选择一个颜色 现在正在用的颜色
-  const settings = reactive({
-    theme: variables.theme,
-    originalTheme: '',
-    t: true,
-  })
-  type ISetting = typeof settings
-  const changeSetting = <T extends keyof ISetting>({ key, value }: { key: T; value: ISetting[T] }) => {
-    settings[key] = value
+export const useSettingStore = defineStore(
+  'setting',
+  () => {
+    // variables
+    // 选择一个颜色 现在正在用的颜色
+    const settings = reactive({
+      theme: variables.theme,
+      originalTheme: '',
+      t: true,
+    })
+    type ISetting = typeof settings
+    const changeSetting = <T extends keyof ISetting>({ key, value }: { key: T; value: ISetting[T] }) => {
+      settings[key] = value
+    }
+    return { changeSetting, settings }
+  },
+  {
+    // 持久化保存
+    persist: {
+      storage: sessionStorage,
+      pick: ['settings.theme'],
+    },
   }
-  return { changeSetting, settings }
-})
+)
