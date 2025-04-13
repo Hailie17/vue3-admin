a6a3f2b10497b4828fc311dabaad596f394aa2f5
Hailie17 <192151452@qq.com>
2025-04-14 01:03:18 +0800

    设置用户主题、颜色、配置 本地化存储

---
+++ src/stores/setting.ts
@@ -0,0 +1,16 @@
+// 设置用户主题、颜色、配置
+import variables from '@/style/variables.module.scss'
+export const useSettingStore = defineStore('setting', () => {
+  // variables
+  // 选择一个颜色 现在正在用的颜色
+  const settings = reactive({
+    theme: variables.theme,
+    originalTheme: '',
+    t: true,
+  })
+  type ISetting = typeof settings
+  const changeSetting = <T extends keyof ISetting>({ key, value }: { key: T; value: ISetting[T] }) => {
+    settings[key] = value
+  }
+  return { changeSetting, settings }
+})
