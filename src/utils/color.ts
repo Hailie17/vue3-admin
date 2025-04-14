50aaf2aa621a1367dbb3d7b81a417439d8adffe5
Hailie17 <192151452@qq.com>
2025-04-15 01:12:29 +0800

    99

---
+++ src/utils/color.ts
@@ -0,0 +1,15 @@
+// 对color做生产，设置
+
+const formula: {[prop:string]:string} = {
+
+}
+
+const generateColors = (primary: string) => {
+  const colors: Record<string,string> => {
+
+  }
+}
+
+const setColors = (colors: Record<string,string>) => {
+  
+}
