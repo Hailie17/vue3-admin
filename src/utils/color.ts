40cd7fd2cc7a34a4f83a7df305a48fa17d9adaa1
Hailie17 <192151452@qq.com>
2025-04-16 01:23:34 +0800

    对color做生产，设置

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,15 +1,32 @@
 // 对color做生产，设置
+import cssFunc from 'css-color-function'
 
-const formula: {[prop:string]:string} = {
-
+const formula: { [prop: string]: string } = {
+  'primary-light-1': 'color(xxx tint(10%))',
+  'primary-light-2': 'color(xxx tint(20%))',
+  'primary-light-3': 'color(xxx tint(30%))',
+  'primary-light-4': 'color(xxx tint(40%))',
+  'primary-light-5': 'color(xxx tint(50%))',
+  'primary-light-6': 'color(xxx tint(60%))',
+  'primary-light-7': 'color(xxx tint(70%))',
+  'primary-light-8': 'color(xxx tint(80%))',
+  'primary-light-9': 'color(xxx tint(90%))',
 }
 
 const generateColors = (primary: string) => {
-  const colors: Record<string,string> => {
-
-  }
+  const colors: Record<string, string> = {}
+  Object.values(formula).forEach((key) => {
+    const value = formula[key].replace(/xxx/g, primary)
+    colors[key] = cssFunc.convert(value)
+  })
+  return colors
 }
 
-const setColors = (colors: Record<string,string>) => {
-  // 111
+const setColors = (colors: Record<string, string>) => {
+  const el = document.documentElement
+  Object.entries(colors).forEach(([key, value]) => {
+    el.style.setProperty(`--el-color-${key}`, value)
+  })
 }
+
+export { generateColors, setColors }
